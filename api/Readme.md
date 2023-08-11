ec8d54aa0a0e38a585ce20e665fa5bbb7381ab01
Tahsin Kabir <tahsinkabir@gmail.com>
2023-08-11 17:35:24 -0400

    some updates to the api service

--- api/Readme.md
+++ api/Readme.md
@@ -9,30 +9,30 @@
 
 ### Create a new UserSetting:
 ```
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer BYPASSTOKEN" -d '{
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer BYPASS_TOKEN" -d '{
     "user_id": "user127",
     "setting_1": "Value 1",
     "setting_2": 42,
     "setting_3": true
-}' http://localhost:8000/v1/usersettings/
+}' http://localhost:80/v1/usersettings/
 ```
 
 ### Get a specific UserSetting by user_id:
 ```
-curl -X GET -H "Authorization: Bearer BYPASSTOKEN" http://localhost:8000/v1/usersettings/{user_id}
+curl -X GET -H "Authorization: Bearer BYPASS_TOKEN" http://localhost:80/v1/usersettings/{user_id}
 ```
 
 ### Update an existing UserSetting by user_id:
 ```
-curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer BYPASSTOKEN" -d '{
+curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer BYPASS_TOKEN" -d '{
     "user_id": "user123",
     "setting_1": "Updated Value",
     "setting_2": 99,
     "setting_3": false
-}' http://localhost:8000/v1/usersettings/{user_id}
+}' http://localhost:80/v1/usersettings/{user_id}
 ```
 
 ### Delete a UserSetting by user_id:
 ```
-curl -X DELETE -H "Authorization: Bearer BYPASSTOKEN" http://localhost:8000/v1/usersettings/{user_id}
+curl -X DELETE -H "Authorization: Bearer BYPASS_TOKEN" http://localhost:80/v1/usersettings/{user_id}
 ```
--- api/test_integration.py
+++ api/test_integration.py
@@ -5,25 +5,25 @@ headers = { "Authorization": "Bearer BYPASS_TOKEN" }
 
 # dependencies must, first be ran: docker-compose up --build
 def test_usersettings_api():
-    base_url = "http://localhost:8000/v1/usersettings"
+    base_url = "http://localhost:80/v1/usersettings"
 
     # CREATE
-    user_settings = UserSettings(user_id="uid_101", setting_1="s1", setting_2=1, setting_3=False)
-    response = requests.post(f"{base_url}/", headers=headers, json=user_settings.model_dump())
+    us = UserSettings(user_id="uid_101", setting_1="s1", setting_2=1, setting_3=False)
+    response = requests.post(f"{base_url}/", headers=headers, json=us.model_dump())
     assert response.status_code == 200
-    assert response.json() == user_settings.model_dump()
+    assert response.json() == us.model_dump()
 
     # READ
-    response = requests.get(f"{base_url}/uid_101", headers=headers)
+    response = requests.get(f"{base_url}/{us.user_id}", headers=headers)
     assert response.status_code == 200
-    assert response.json() == user_settings.model_dump()
+    assert response.json() == us.model_dump()
 
     # UPDATE
-    user_settings.setting_1="s2"
-    response = requests.put(f"{base_url}/uid_101", headers=headers, json=user_settings.model_dump())
+    us.setting_1="s2"
+    response = requests.put(f"{base_url}/{us.user_id}", headers=headers, json=us.model_dump())
     assert response.status_code == 200
-    assert response.json() == user_settings.model_dump()
+    assert response.json() == us.model_dump()
 
     # DELETE
-    response = requests.delete(f"{base_url}/uid_101", headers=headers)
+    response = requests.delete(f"{base_url}/{us.user_id}", headers=headers)
     assert response.status_code == 200
