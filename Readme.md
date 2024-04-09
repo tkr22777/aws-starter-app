c5b08083c0e48f63aa2e62b3466182d46044105b
Tahsin Kabir <tahsinkabir@gmail.com>
2024-04-09 16:34:17 -0400

    some updates to the naming and excluding db access for now

--- Readme.md
+++ Readme.md
@@ -1,7 +1,11 @@
 TODO:
+* Set up ECS
+* Route to public
+* Frontend UI
+* FE with authentication
+
+
+# Using ECS as it is easier to start off:
 * EKS Based API route
 * Setting up an EKS cluster, setting up pods and scaling it
 * Container based service/job deployment in EKS
-* Routing them to public
-* Frontend UI
-* FE with authentication
--- api/main.py
+++ api/main.py
@@ -3,5 +3,5 @@ from controllers import usersettings_controller, admin_controller
 
 app = FastAPI()
 
-app.include_router(usersettings_controller.router, prefix="/v1/usersettings", tags=["usersettings"])
+# app.include_router(usersettings_controller.router, prefix="/v1/usersettings", tags=["usersettings"])
 app.include_router(admin_controller.router, prefix="/v1/admin", tags=["admin"])
