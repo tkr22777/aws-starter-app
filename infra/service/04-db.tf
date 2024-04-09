31385d219f73d204aa5b1be0cdb6d8009d37d9e1
Tahsin Kabir <tahsinkabir@gmail.com>
2024-04-09 16:34:39 -0400

    excluding creating ec2 and rds

--- infra/service/04-db.tf
+++ infra/service/04-db.tf
@@ -43,27 +43,27 @@ resource "aws_db_subnet_group" "rds_sng" {
   ] 
 }
 
-# RDS instance
-resource "aws_db_instance" "my_database" {
-  allocated_storage    = 20                    # 20 GB, adjust as needed
-  storage_type         = "gp2"                 # General Purpose storage
-  engine               = "postgres"            # PostgreSQL engine
-  engine_version       = "13.11"               # Specific PostgreSQL version
-  instance_class       = "db.t3.micro"         # Instance class, choose based on needs
-  db_name              = "postgres"            # DB name
-  username             = "postgres"            # DB username
-  password             = "abcd1234"            # DB password
-  parameter_group_name = "default.postgres13"  # Default parameter group for PostgreSQL 13
+# # RDS instance
+# resource "aws_db_instance" "my_database" {
+#   allocated_storage    = 20                    # 20 GB, adjust as needed
+#   storage_type         = "gp2"                 # General Purpose storage
+#   engine               = "postgres"            # PostgreSQL engine
+#   engine_version       = "13.11"               # Specific PostgreSQL version
+#   instance_class       = "db.t3.micro"         # Instance class, choose based on needs
+#   db_name              = "postgres"            # DB name
+#   username             = "postgres"            # DB username
+#   password             = "abcd1234"            # DB password
+#   parameter_group_name = "default.postgres13"  # Default parameter group for PostgreSQL 13
 
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  db_subnet_group_name = aws_db_subnet_group.rds_sng.name
+#   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+#   db_subnet_group_name = aws_db_subnet_group.rds_sng.name
 
-  skip_final_snapshot  = true                  # Skip final snapshot when deleting the DB
-  multi_az             = false                 # Set to true for multi-AZ deployment
-  publicly_accessible  = false                 # Set to false to prevent public access
-}
+#   skip_final_snapshot  = true                  # Skip final snapshot when deleting the DB
+#   multi_az             = false                 # Set to true for multi-AZ deployment
+#   publicly_accessible  = false                 # Set to false to prevent public access
+# }
 
-output "db_endpoint" {
-  description = "The connection endpoint for the database"
-  value       = aws_db_instance.my_database.endpoint
-}
+# output "db_endpoint" {
+#   description = "The connection endpoint for the database"
+#   value       = aws_db_instance.my_database.endpoint
+# }
