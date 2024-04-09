9aba2b695c06dcdd38dfd233905b8bd90c7606bb
Tahsin Kabir <tahsinkabir@gmail.com>
2024-04-09 13:57:01 -0400

    added admin controller with ping-pong route

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from controllers import usersettings_controller
+from controllers import usersettings_controller, admin_controller
 
 app = FastAPI()
 
 app.include_router(usersettings_controller.router, prefix="/v1/usersettings", tags=["usersettings"])
+app.include_router(admin_controller.router, prefix="/v1/admin", tags=["admin"])
